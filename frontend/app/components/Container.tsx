ac7fa3524aa544301de5175403a7dd1b77ebb5fd
Andriy Kostenko <a.kostenkouk@gmail.com>
2023-11-29 19:23:53 -0500

    created the basic container for central section

--- frontend/app/components/Container.tsx
+++ frontend/app/components/Container.tsx
@@ -6,7 +6,11 @@ interface ContainerProps{
 // will be used as a middle section of page
 const Container: React.FC<ContainerProps> = ({children}) => {
     return ( 
-        <div>
+        <div className="max-w-[1920px] 
+                        mx-auto 
+                        xl:px-20
+                        md:px-2
+                        px-4">
             {children}
         </div>
      );
