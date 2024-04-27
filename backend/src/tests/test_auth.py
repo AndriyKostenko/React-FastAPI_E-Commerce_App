452e2b28791b6e7e838661552cc0ce3b58b27805
Andriy Kostenko <a.kostenkouk@gmail.com>
2024-04-26 18:41:36 -0600

    moooore tests

--- backend/src/tests/test_auth.py
+++ backend/src/tests/test_auth.py
@@ -1,23 +1,28 @@
+import pytest
 from jose import jwt
-from datetime import timedelta
-
 from src.config import settings
-from src.security.authentication import create_access_token
+from src.security.authentication import get_current_user
+from fastapi import HTTPException
+# marking with package to use same event_loop() for all tests in package
+pytestmark = pytest.mark.asyncio(scope="package")
 
 
-def test_create_access_token():
-    username = 'test_user'
-    user_id = 1
-    role = 'admin'
-    expires_delta = timedelta(minutes=15)
+async def test_get_current_user():
+    encode = {'sub': 'test_user',
+              'id': 1,
+              'role': 'admin'}
+    token = jwt.encode(encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    user = await get_current_user(token)
 
-    token = create_access_token(username, user_id, role, expires_delta)
+    assert user == {'email': 'test_user', 'id': 1, 'user_role': 'admin'}
 
-    decoded_token = jwt.decode(token,
-                               settings.SECRET_KEY,
-                               algorithms=[settings.ALGORITHM],
-                               options={"verify_signature": False})
 
-    assert decoded_token.get('sub') == username
-    assert decoded_token.get('id') == user_id
-    assert decoded_token.get('role') == role
+async def test_get_current_user_invalid_token():
+    encode = {'role': 'user'}
+    token = jwt.encode(encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+    with pytest.raises(HTTPException) as exceptionInfo:
+        await get_current_user(token=token)
+
+    assert exceptionInfo.value.status_code == 401
+    assert exceptionInfo.value.detail == 'Could not validate user'
