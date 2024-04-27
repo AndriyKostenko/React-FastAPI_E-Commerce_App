9f0a04f4a70c2078d5ce18bbb4c045fe06932e87
Andriy Kostenko <a.kostenkouk@gmail.com>
2024-04-26 18:17:44 -0600

    one more sml test for creatig of token is added

---
+++ backend/src/tests/test_auth.py
@@ -0,0 +1,23 @@
+from jose import jwt
+from datetime import timedelta
+
+from src.config import settings
+from src.security.authentication import create_access_token
+
+
+def test_create_access_token():
+    username = 'test_user'
+    user_id = 1
+    role = 'admin'
+    expires_delta = timedelta(minutes=15)
+
+    token = create_access_token(username, user_id, role, expires_delta)
+
+    decoded_token = jwt.decode(token,
+                               settings.SECRET_KEY,
+                               algorithms=[settings.ALGORITHM],
+                               options={"verify_signature": False})
+
+    assert decoded_token.get('sub') == username
+    assert decoded_token.get('id') == user_id
+    assert decoded_token.get('role') == role
